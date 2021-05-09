16e091fb8e505a3b7417c61abe5542541e1887e5
Sakshham Bhagat <sakshhamtg@gmail.com>
2021-05-09 13:29:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var attempt = 3
+function validater() {
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if ((username == "sakshhamtg@gmail.com" && password == "password@2020") ||( username=="heermithu@gmail.com" && password=="heeranaya1")){
+        alert('You have logined as '+ username) // Redirecting to other page.
+        return true;
+    } else {
+
+        alert("You have left attempt;");
+        attempt = attempt - 1;
+        // Disabling fields after 3 attempts.
+        // if( attempt == 0){
+        // document.getElementById("username").disabled = true;
+        // document.getElementById("password").disabled = true;
+        // document.getElementById("submit").disabled = true;
+        // alert('account blocked plz reload')
+        return false;
+    }
+}
+// }
